49c481f237b585a6c8dda9191252b9396e0944aa
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-03-08 08:05:00 +0530

    practical

---
+++ q4.c
@@ -0,0 +1,15 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/*
+Write a C program using functions to read an m x n matrix and find all saddle points and to find second largest element without sorting. Hint: A saddle point is an element which is smallest in its row but largest in its column. If no saddle point exists, display an appropriate message. 
+i/p (Input):
+3 1 3
+3 2 4
+0 6 5
+o/p (Output):
+Saddle Point = 3 at position 1st row 1st column.
+*/
